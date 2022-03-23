d56b33eb3ceec006f357aed8b738c76e23b99ef7
TN03 <h.irmler@t-online.de>
2022-03-23 12:16:57 +0100

    Standard-Konfiguration angepasst
    siehe https://github.com/TN03/expandcontract/commit/be176a547d6939039e7620cdf245ab107fdd8e08

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 $plugin_cf['expandcontract']['use_inline_buttons']="";
 $plugin_cf['expandcontract']['use_stylesheet']="example1.css";
-$plugin_cf['expandcontract']['expand-content_padding']="20px";
+$plugin_cf['expandcontract']['expand-content_padding']="";
 $plugin_cf['expandcontract']['expand-content_first_open']="";
 $plugin_cf['expandcontract']['expand-content_auto_close']="true";
 $plugin_cf['expandcontract']['expand-content_show_close_button']="true";
