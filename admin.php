4b1747245626ca185a10608a6a7cb1c8694f99a2
Olaf Penschke <35469568+olape-git@users.noreply.github.com>
2025-08-08 09:50:31 +0200

    System check is missing
    https://github.com/cmsimple-xh/expandcontract/issues/11

--- admin.php
+++ admin.php
@@ -19,7 +19,7 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
  * Registers the plugin menu items
  */
 if (function_exists('XH_registerStandardPluginMenuItems')) {
-    XH_registerStandardPluginMenuItems(true);
+    XH_registerStandardPluginMenuItems(false);
 }
 
 if (function_exists('XH_wantsPluginAdministration')
@@ -27,19 +27,101 @@ if (function_exists('XH_wantsPluginAdministration')
         || isset($expandcontract) && $expandcontract == 'true')
 {
     $o .= print_plugin_admin('on');
+//**************************************************************************
+//**************************************************************************
+    $ec_pluginName = 'Expandcontract_XH';
+    $ec_pluginVersion = '1.1';
+    $ec_copyright = '2022';
+    $ec_cmsVersionArray = array('1.7.3', 'and higher');
+    $ec_phpVersion = '7.4';
+//**************************************************************************
+//**************************************************************************
+    // display only on start page of plugin and under info
     if (!$admin || $admin == 'plugin_main') {
-        $o .= '<h1>Expandcontract_XH</h1>'
-            . '<p>Version 1.0 - 2022.03.31'
-            . '<br>&copy; 2014-16 <a href="http://svasti.de" target="_blank">svasti</a>'
-            . '<br>&copy; 2022 <a href="https://www.cmsimple-xh.org/" target="_blank">The CMSimple_XH Community</a>
+        $o .= '<img class="ec_admin_logo" alt="Logo ' . $ec_pluginName . '" src="'
+            . $pth['folder']['plugins'] . 'expandcontract/img/expandcontract-logo.svg">'
+            . "\n";
+        $o .= '<h1>' . $ec_pluginName . '</h1>'
+            . '<p>Version ' . $ec_pluginVersion
+            . '<br>&copy; 2014 - 2016 <a href="http://svasti.de" target="_blank">svasti</a>'
+            . '<br>&copy; ' . $ec_copyright . ' <a href="https://www.cmsimple-xh.org/" target="_blank">The CMSimple_XH Community</a>
             . <br>Licence: <a target="_blank" href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a></p>'
-            . '<p>'. $plugin_tx['expandcontract']['plugin_explanation']. '</p>'
-            . '<h2>'.$plugin_tx['expandcontract']['plugin_call'].'</h2>'
-            . '<p>'. $plugin_tx['expandcontract']['link_hidden_subpages']. '</p>'
-            . '<p>'. $plugin_tx['expandcontract']['link_multiple_pages']. '</p>'
-            . '<p>'. $plugin_tx['expandcontract']['link_single_page']. '</p>';
+            . '<p>'. $plugin_tx['expandcontract']['plugin_explanation']. '</p>';
+
+//**************************************************************************
+// System check
+//**************************************************************************
+        $o .= '<h2>System Check</h2>' . "\n";
+// CMSimple_XH Version
+        $ec_cmsVersionTmp = CMSIMPLE_XH_VERSION;
+        $ec_cmsVersionTmp = str_replace(array('CMSimple_XH '), '', $ec_cmsVersionTmp);
+        if (version_compare($ec_cmsVersionTmp, $ec_cmsVersionArray[0], '<'))
+        {
+            $o .= '<p class="xh_warning">'
+                . CMSIMPLE_XH_VERSION
+                . ' &#x2192 I hope it is still supported. It was designed for '
+                . $ec_cmsVersionArray[0]
+                . ' - '
+                . end($ec_cmsVersionArray)
+                . '.</p>'
+                . "\n";
+        }
+        else
+        {
+            $o .= '<p class="xh_success">'
+                . CMSIMPLE_XH_VERSION
+                . ' &#x2192 supported</p>'
+                . "\n";
+        }
+// PHP Version
+        if (version_compare(phpversion(), $ec_phpVersion, '<')) {
+            $o .= '<p class="xh_fail">PHP: '
+                . phpversion()
+                . ' &#x2192 not supported</p>'
+                . "\n";
+        } else {
+            $o .= '<p class="xh_success">PHP: '
+                . phpversion()
+                . ' &#x2192 supported</p>'
+                . "\n";
+        }
+// write permissions
+        $ec_fileNameArray = array($pth['file']['plugin_stylesheet'],
+                                  $pth['file']['plugin_config'],
+                                  $pth['folder']['plugin_languages'],
+                                  $pth['file']['plugin_language']);
+        foreach ($ec_fileNameArray as $ec_fileName) {
+            if (is_writable($ec_fileName)) {
+                $o .= '<p class="xh_success">'
+                    . $ec_fileName
+                    . ' &#x2192 writable</p>'
+                    . "\n";
+            } else {
+                $o .= '<p class="xh_fail">'
+                    . $ec_fileName
+                    . ' &#x2192 not writable</p>'
+                    . "\n";
+            }
+        }
+// checks access protection
+        if (XH_isAccessProtected($pth['file']['plugin_config']) === true) {
+            $o .= '<p class="xh_success"><a target="_blank" href="'
+                . $pth['file']['plugin_config']
+                . '">'
+                . $pth['file']['plugin_config']
+                . '</a> &#x2192 protected</p>'
+                . "\n";
+        } else {
+            $o .= '<p class="xh_warning"><a target="_blank" href="'
+                . $pth['file']['plugin_config']
+                . '">'
+                . $pth['file']['plugin_config']
+                . '</a> &#x2192 protected ?</p>'
+                . "\n";
+        }
+        $o .= '</div>' . "\n";
     }
-	$o .= plugin_admin_common($action, $admin, $plugin);
+    $o .= plugin_admin_common($action, $admin, $plugin);
 }
 
 /**
--- languages/de.php
+++ languages/de.php
@@ -3,10 +3,6 @@
 $plugin_tx['expandcontract']['close']="schließen";
 $plugin_tx['expandcontract']['menu_main']="Über";
 $plugin_tx['expandcontract']['plugin_explanation']="Expandcontract_XH ermöglicht die Anzeige anderer CMSimple_XH-Seiten innerhalb einer Seite. Die Anzeige kann aufgeklappt (<em>expand</em>) und wieder geschlossen (<em>contract</em>) werden.";
-$plugin_tx['expandcontract']['plugin_call']="Pluginaufrufe";
-$plugin_tx['expandcontract']['link_hidden_subpages']="<code>{{{expand}}}</code><br><i>Erzeugt Expand-Links zu allen verstecken Unterseiten einer Seite.<br>Die Links werden (abhängig von der Konfiguration) entweder als mehrere Buttons nebeneinander in einer Zeile angezeigt oder als Text-Links untereinander in jeweils einem eigenen Absatz.</i>";
-$plugin_tx['expandcontract']['link_single_page']="<code>{{{expand('pages = Seite X', 'headlines = Linktext')}}}</code><br><i>Erzeugt einen Expand-Link mit angegebenem \"Linktext\" zur Seite \"Seite X\".</i>";
-$plugin_tx['expandcontract']['link_multiple_pages']="<code>{{{expand('pages = Seite 1, Seite 2, Seite 3, Seite 4')}}}</code><br><i>Erzeugt Expand-Links zu den Seiten \"Seite 1\", \"Seite 2\", \"Seite 3\", \"Seite 4\".</i>";
 $plugin_tx['expandcontract']['cf_use_inline_buttons']="EIN: Es werden Buttons als Expand-Links verwendet die sich in einer Zeile nebeneinander anordnen.<br>AUS: Die Expand-Links sehen wie normale Links aus, die sich untereinander anordnen.";
 $plugin_tx['expandcontract']['cf_use_stylesheet']="LEER =<br>Es wird nur die stylesheet.css des Plugins verwendet die nur grundlegende Regeln enthält.<br>\"example1.css\" =<br>Zusätzlich wird die angegebene Stylesheet-Datei eingebunden, mit der das Aussehen insgesamt individuell gestaltet werden kann.<br>\"example1.css\" ist eine mitgelieferte Beispiel-Datei. Es kann hier eine beliebige eigene Datei verwendet werden, die im Plugin-CSS-Ordner liegen muss.";
 $plugin_tx['expandcontract']['cf_expand-content_padding']="Will man den expandierten Bereich z.B. mit einem Hintergrund oder einem Rahmen versehen, empfiehlt es sich, hier einen Randabstand <i>(padding)</i> einzutragen. Erlaubt sind alle validen CSS-Werte.";
